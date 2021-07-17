8f929292e05af9d1f42687db29cfa376a034ebdd
Felipe Augusto <felipe.augusto.1303@gmail.com>
2021-07-16 23:05:34 -0400

    DB configs

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize'
+import Library from './components/Library/library.model';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'library',
+      autoLoadModels: true,
+      synchronize: true
+    }),
+    SequelizeModule.forFeature([Library])
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
