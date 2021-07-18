e6f0968769916a070c7453e3495256ebfdf6864d
Felipe Augusto <felipe.augusto.1303@gmail.com>
2021-07-17 23:42:21 -0400

    Adding bookshelf page ate app.js

--- mobile/App.js
+++ mobile/App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Bookshelf from './screens/Bookshelf';
+import {
+  SafeAreaView
+} from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hello world!</Text>
+    <SafeAreaView style={styles.container}>
+      <Bookshelf/>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +19,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
